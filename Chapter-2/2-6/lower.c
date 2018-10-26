0707b637bd7355bb07b8c4da16d773489d010c1d
Braam <braam7beresford@gmail.com>
2018-10-26 11:22:24 -0700

    To lower with unary

---
+++ Chapter-2/2-6/lower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void lower(char s[]);
+int main(){
+	char test[] = "HelLo there!";		
+	lower(test);
+	printf("%s \n", test);
+}
+
+void lower(char s[]){
+	int i;
+
+	for(i = 0; s[i] != '\0'; i++){
+		(s[i] >= 'A' && s[i] <= 'Z') ? s[i] += 'a' - 'A': s[i];
+	}
+}
